7030182a55879b203a9ee832b6a2c6f8dff712ea
nanna <navkishoredas@gmail.com>
2022-12-23 23:24:45 +0530

    application yaml file

--- src/main/java/com/nanna/demo/DemoContrlloer.java
+++ src/main/java/com/nanna/demo/DemoContrlloer.java
@@ -1,5 +1,8 @@
 package com.nanna.demo;
 
+import org.springframework.beans.PropertyAccessException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,11 +13,25 @@ public class DemoContrlloer {
     
     
     private static  String SENTENCE = "Hello World ";
+    private final String propertySentence;
     
+    @Autowired
+    public DemoContrlloer ( @Value ( "${demo.sentence}" ) String propertySentence) {
+    
+        this.propertySentence = propertySentence;
+    
+    }
     
     @RequestMapping (value = "/hello", method = RequestMethod.GET)
     public String helloWorld() {
         return SENTENCE;
+    }
+    
+    @RequestMapping(value = "property/sentence", method = RequestMethod.GET)
+    public String helloFromProperty() {
+    
+    
+        return propertySentence;
     }
     
     @RequestMapping(value = "update/sentence", method = RequestMethod.POST)
