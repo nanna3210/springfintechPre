751fb2bd631a8913b9194585a555ea4bf4b4b59c
nanna <navkishoredas@gmail.com>
2022-12-25 21:57:39 +0530

    config bean comment!

--- src/main/java/com/nanna/demo/ApplicationConfig.java
+++ src/main/java/com/nanna/demo/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.nanna.demo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//config class
 @Configuration
 public class ApplicationConfig {
     
