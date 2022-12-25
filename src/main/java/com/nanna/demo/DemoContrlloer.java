630dcc32a13e0cf74d5283ed2062585ce4c041ed
nanna <navkishoredas@gmail.com>
2022-12-25 21:59:14 +0530

    controller  comment!

--- src/main/java/com/nanna/demo/DemoContrlloer.java
+++ src/main/java/com/nanna/demo/DemoContrlloer.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoContrlloer {
     
-    
+//    controller class
     private static  String SENTENCE = "Hello World ";
     private final String propertySentence;
     
