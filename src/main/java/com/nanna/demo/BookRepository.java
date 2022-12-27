2c05feacaf23100a1ee41e6875ba43bb626633e6
nanna <navkishoredas@gmail.com>
2022-12-27 23:08:08 +0530

    comment

--- src/main/java/com/nanna/demo/BookRepository.java
+++ src/main/java/com/nanna/demo/BookRepository.java
@@ -1,10 +1,10 @@
 package com.nanna.demo;
 
 import java.util.List;
-
+//repository
 public class BookRepository {
 
-
+//
     public List<String> findAllBooks() {
     
         return List.of ( "sapiens" , "harry porter" );
